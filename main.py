01f2ce636e09dab5ed6f92311ae3d60e8ca8a917
KanVull <minirorus7@gmail.com>
2021-05-08 17:08:16 +0300

    ended ui (resized + check for not downloaded)

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ import webbrowser
 import work_xml
 
 class TrackWidget(QWidget):
-    def __init__(self, Form, track):
-        QWidget.__init__(self, parent=Form)
+    def __init__(self, track, parent=None):
+        QWidget.__init__(self, parent=parent)
 
         self.track = track
         prepairedName = self.track.name.lower().replace('&', '%26')
@@ -24,48 +24,68 @@ class TrackWidget(QWidget):
         self.setUI()
 
     def setUI(self):
-        self.mainlayout = QHBoxLayout()
+        self.setMinimumSize(QSize(0,70))
+        self.setMaximumSize(QSize(16777215, 70))
+
+        self.widgetlayout = QHBoxLayout(self)
 
         self.layoutnames = QVBoxLayout()
-        self.layoutnames.setAlignment(QtCore.Qt.AlignLeft)
+        self.layoutnames.setSpacing(0)
         self.layoutnames.setObjectName('layoutnames')
-        self.labelname = QLabel()
+
+        self.labelname = QLabel(self)
+        self.labelname.setTextFormat(Qt.MarkdownText)
+        self.labelname.setScaledContents(True)
+        self.labelname.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
+        self.labelname.setWordWrap(True)
+        self.labelname.setMargin(0)
+        self.labelname.setIndent(-1)
         self.labelname.setText(self.track.name)
         self.labelname.setObjectName('labelname')
-        self.labelartist = QLabel()
+
+        self.labelartist = QLabel(self)
         self.labelartist.setText(self.track.artist)
         self.labelartist.setObjectName('labelartist')
-        self.labelplaylists = QLabel()
+
+        self.labelplaylists = QLabel(self)
         self.labelplaylists.setText(', '.join(self.track.playlists))
         self.labelplaylists.setObjectName('labelplaylists')
+
         self.layoutnames.addWidget(self.labelname)
         self.layoutnames.addWidget(self.labelartist)
         self.layoutnames.addWidget(self.labelplaylists)
+        self.layoutnames.setContentsMargins(0,0,0,0)
         
+
         self.layoutButtonsInfo = QVBoxLayout()
-        self.layoutButtonsInfo.setAlignment(QtCore.Qt.AlignRight)
         self.layoutButtonsInfo.setObjectName('layoutbuttonsinfo')
-        self.labeltime = QLabel()
+
+        self.labeltime = QLabel(self)
         self.labeltime.setText(self.track.time)
         self.labeltime.setObjectName('labeltime')
-        self.checkout = QCheckBox()
+
+        self.checkout = QCheckBox(self)
         self.checkout.setObjectName("checkbox")
         self.checkout.setToolTip('downloaded')
         if self.track.downloaded:
             self.checkout.setChecked(True)   
-        self.checkout.stateChanged.connect(self.changeCheckStatus)    
-        self.labelinfo = QLabel()
+        self.checkout.stateChanged.connect(self.changeCheckStatus)   
+
+        self.labelinfo = QLabel(self)
         self.labelinfo.setText(f'Добавлен: {self.track.date}')
         self.labelinfo.setObjectName('labelinfo')
+
         self.layoutButtonsInfo.addWidget(self.labeltime)
         self.layoutButtonsInfo.addWidget(self.checkout)
         self.layoutButtonsInfo.addWidget(self.labelinfo)
+        self.layoutButtonsInfo.setContentsMargins(0,0,0,0)
 
-        self.mainlayout.addLayout(self.layoutnames)
-        self.mainlayout.addStretch(1)
-        self.mainlayout.addLayout(self.layoutButtonsInfo)
-        self.setLayout(self.mainlayout)
 
+        self.widgetlayout.addLayout(self.layoutnames)
+        self.widgetlayout.addLayout(self.layoutButtonsInfo)
+        self.widgetlayout.setStretch(0, 50)
+        self.widgetlayout.setContentsMargins(6,3,6,3)
+        
         self.setAttribute(QtCore.Qt.WA_StyledBackground, True)
         self.setObjectName("trackwidget")
         self.setToolTip(f'{self.track.get_name()}\n(double click to copy to clipboard)')
@@ -75,10 +95,11 @@ class TrackWidget(QWidget):
 
         self.setContextMenuPolicy(Qt.ActionsContextMenu)
         for key in self.links.keys():
-            action = QAction()
+            action = QAction(self)
             action.setText(key)
             action.triggered.connect(self.download)
             self.addAction(action)
+    
 
     def download(self):
         sender = self.sender()
@@ -125,57 +146,80 @@ class TrackWidget(QWidget):
     def changeCheckStatus(self, state):
         if state == Qt.Checked:
             work_xml.changeStatus(self.track.track_id, True)
+            self.track.downloaded = True
+            main = self.parent()
+            while type(main) != Main:
+                main = main.parent()
+            if main.filterDownloaded.checkState() == Qt.Checked:
+                self.hide()
         else:
             work_xml.changeStatus(self.track.track_id, False)
+            self.track.downloaded = False  
         
 
 class Ui_Form(object):
-    def createWidget(self, Form, track):
-        widget = TrackWidget(Form, track)
-        return widget
-
-    def setupUi(self, Form):
-        Form.setWindowTitle("Your Spoty")
-        Form.setObjectName("form")
-        Form.resize(600, 800)
-        Form.setAttribute(QtCore.Qt.WA_StyledBackground, True)
+    def setupUi(self, MainWindow):
+        MainWindow.setWindowTitle("Your Spoty")
+        MainWindow.setObjectName("form")
+        MainWindow.resize(800, 800)
+        MainWindow.setAttribute(QtCore.Qt.WA_StyledBackground, True)
         tracks = work_xml.Load_list()
 
-        self.layout = QVBoxLayout()
-        for i, track in enumerate(tracks):
-            self.layout.addWidget(self.createWidget(Form, track))
+        self.centralWidget = QWidget(MainWindow)
+        self.centralWidget.setObjectName('centralwidget')
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setContentsMargins(0,0,0,0)
+        self.centralWidgetLayout = QVBoxLayout(self.centralWidget)
+        self.centralWidgetLayout.setContentsMargins(0,0,0,0)
 
-        self.layoutTracks = QHBoxLayout()
-        self.tracks = QWidget()
-        self.tracks.setObjectName('listwidget')
-        self.tracks.setLayout(self.layout)
-        self.layoutTracks.addWidget(self.tracks)
-
-        self.scrollArea = QScrollArea(Form)
-        self.scrollArea.setGeometry(QtCore.QRect(0, 0, Form.size().width(), Form.size().height() - 40))
-        self.scrollArea.setLayout(self.layoutTracks)
+        self.scrollArea = QScrollArea(self.centralWidget)
         self.scrollArea.setObjectName("scrollarea")
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollAreaWidgetContents = QWidget()
+        self.scrollAreaWidgetContentsLayout = QVBoxLayout(self.scrollAreaWidgetContents)
+        self.scrollAreaWidgetContentsLayout.setContentsMargins(0,0,0,0)
+        self.scrollAreaWidgetContentsLayout.setSpacing(0)
+        for track in tracks:
+            self.scrollAreaWidgetContentsLayout.addWidget(TrackWidget(track))
+            
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)    
 
-class Main(QtWidgets.QWidget, Ui_Form):
-    resized = QtCore.pyqtSignal() 
+        self.filtersLayout = QHBoxLayout()
+        self.filtersLayout.setContentsMargins(10,0,0,8)
+        self.filterDownloaded = QCheckBox(self.centralWidget) 
+        self.filterDownloaded.setText('Only not downloaded')
+        self.filterDownloaded.setToolTip('Show only not downloaded') 
+        self.filterDownloaded.stateChanged.connect(self.changeCheckStatus) 
+        self.filtersLayout.addWidget(self.filterDownloaded)
+
+        self.verticalLayout.addWidget(self.scrollArea)
+        self.verticalLayout.addLayout(self.filtersLayout)
+        self.verticalLayout.setContentsMargins(0,0,0,0)
+        self.centralWidgetLayout.addLayout(self.verticalLayout)
+        self.centralWidgetLayout.setContentsMargins(0,0,0,0)
+
+        MainWindow.setCentralWidget(self.centralWidget)
+        MainWindow.setContentsMargins(0,0,0,0)
+
+    def changeCheckStatus(self, state):
+        for widget in self.scrollArea.widget().children():
+            if type(widget) != TrackWidget:
+                continue
+            if state == Qt.Checked:
+                if widget.track.downloaded == True:
+                    widget.hide()
+            else:
+                widget.show()
+
+        
+
+class Main(QMainWindow, Ui_Form):
     def __init__(self, parent=None):
         super(Main, self).__init__(parent) 
-        self.setupUi(self)
-        self.resized.connect(self.sizeChanged)
-        
-    def resizeEvent(self, event):
-        self.resized.emit()
-        return super(Main, self).resizeEvent(event)
-
-    def sizeChanged(self):
-        self.scrollArea.setGeometry(QtCore.QRect(0, 0, self.size().width(), self.size().height() - 40))           
+        self.setupUi(self)        
 
 if __name__ == '__main__':
     styleWidget = '''
-            #layoutbuttonsinfo, #layoutnames {
-                width: 50%;
-                display: flex;
-            }
             #scrollarea, #trackwidget, #listwidget {
                 background-color: #ffffff;
                 border-width: 0;
@@ -184,9 +228,6 @@ if __name__ == '__main__':
             }
             #trackwidget:hover {
                 background-color: #efefef;
-            }
-            #checkbox {
-                border-radius: 10px;
             }
             #labelname, #labeltime {
                 font-weight: bold;
